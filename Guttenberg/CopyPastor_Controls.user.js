c31c4d22236756a89b64cfccf17d47785b19c540
paper1111 <30367398+paper1111@users.noreply.github.com>
2018-01-31 17:43:49 +0800

    Add text insert

--- Guttenberg/CopyPastor_Controls.user.js
+++ Guttenberg/CopyPastor_Controls.user.js
@@ -19,6 +19,17 @@ var containers = document.getElementsByClassName("container");
 containers.forEach( function(container) {
     var rows = container.getElementsByClassName("row");
     rows.forEach( function(row) {
-    
-    }
+        var cols = row.getElementsByClassName("col");
+        cols.forEach( function(col) {
+            var titles = col.getElementsByTagName("h2")
+            titles.forEach( function(title) {
+                var links = col.getElementsByTagName("a")
+                links.forEach( function(link) {
+                    if (link.textContent === "Possible Plagiarism") {
+                        titles += buttonSpan;
+                    }
+                });
+            });
+        });
+    });
 });
