ef0be9863498a159286f12fad54493eb51b35552
paper1111 <30367398+paper1111@users.noreply.github.com>
2018-01-31 18:35:18 +0800

    change to one-line string

--- Guttenberg/CopyPastor_Controls.user.js
+++ Guttenberg/CopyPastor_Controls.user.js
@@ -3,17 +3,12 @@
 // @namespace   paper1111
 // @description Adds [k] [f] buttons to CopyPastor
 // @include     http://copypastor.sobotics.org/posts/*
-// @version     0.4
+// @version     0.1.5
 // @grant       none
 // ==/UserScript==
 
 var buttonSpan = document.createElement ('span');
-buttonSpan.innerHTML   = '          \
-    <span id="controls">            \
-        <a>k</a>                    \
-        <a>f</a>                    \
-    </span>                         \
-';
+buttonSpan.innerHTML   = '<span id="controls"><a>k</a><a>f</a></span>';
 
 var containers = document.getElementsByClassName("container");
 [].forEach.call(containers, function(container) {
